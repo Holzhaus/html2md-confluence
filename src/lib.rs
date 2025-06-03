6da7e2968664506883d2100d64295b6f7537bd84
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2025-06-04 00:09:58 +0200

    chore: Decode CData sections using quick-xml

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,31 @@ mod util;
 use html2md::{TagHandlerFactory, parse_html_custom};
 use std::collections::HashMap;
 use util::{ConfluencePageId, ConfluenceServer, JiraServer, JiraServerMap};
+use quick_xml::{
+    errors::Result,
+events::Event,
+reader::Reader,
+writer::Writer,
+};
+use std::io::{BufRead, Write};
+
+
+fn remove_cdata<R: BufRead, W: Write>(
+    reader: &mut Reader<R>,
+    writer: &mut Writer<W>,
+) -> Result<()> {
+    let mut buf = Vec::with_capacity(2048);
+
+    loop {
+        let event = match reader.read_event_into(&mut buf)? {
+            Event::CData(text) => Event::Text(text.escape()?),
+            Event::Eof => break Ok(()),
+            other_event => other_event,
+        };
+
+        writer.write_event(event.borrow())?;
+    }
+}
 
 #[derive(Debug, Default, Clone)]
 pub struct ParseOptions {
@@ -54,5 +79,13 @@ pub fn parse_confluence<S: AsRef<str>>(source: S, options: &ParseOptions) -> Str
             options.server.clone().zip(options.page_id.clone()),
         )),
     );
-    parse_html_custom(source.as_ref(), &handlers)
+
+    let mut reader = Reader::from_str(source.as_ref());
+    let mut buffer = Vec::new();
+    let mut writer = Writer::new(&mut buffer);
+    remove_cdata(&mut reader, &mut writer).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    parse_html_custom(&text, &handlers)
 }
